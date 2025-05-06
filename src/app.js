4fd424d8b32c87460c9649c5fdcaa2fe837e5c9e
NataliiaSe <156771694+NataliiaSe@users.noreply.github.com>
2025-05-06 11:14:47 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -213,7 +213,11 @@ app.get('/playlist/:id/tracks', async (req, res) => {
     }
   });
   
-
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+  app.get('/', (req, res) => {
+    res.send('🎵 Music AI backend is running!');
+  });
+  
+  app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+  });
+  
